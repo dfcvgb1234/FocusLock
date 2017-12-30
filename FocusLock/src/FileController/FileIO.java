ac7864b11de3911d9b67eef133a5fc040027bf9c
dfcvgb1234 <www.favs2@gmail.com>
2017-12-30 22:17:35 +0100

    Det rigtige FocusLock projekt er oprettet og den gamle kode er implementeret

--- FocusLock/src/FileController/FileIO.java
+++ FocusLock/src/FileController/FileIO.java
@@ -9,23 +9,29 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ *
+ * @author InsertName
+ */
 public class FileIO
 {
 	// Path til filen der skal læses fra
 	Path path;
-
+    
+	// Laver en liste med alle linjerne i
 	List<String> lines;
-
+    
 	// Constructor til class
 	public FileIO(String path)
 	{
 		// Laver en ny liste med alle linjerne i filen (ny fil-struktur for program filen)
 		this.lines = new ArrayList<>();
 		this.path = Paths.get(path);
-
+        
 		// Checker om filen eksistere, hvis ikke lav den
 		if(!Files.exists(this.path))
 		{
+			// Fortæller om der er sket en fejl
 			try 
 			{
 				Files.createFile(this.path);
@@ -36,10 +42,11 @@ public class FileIO
 			}
 		}
 	}
-
+    
 	// Fylder den generede liste med linjerne og retunere den
 	public List<String> GenerateLineList() throws IOException
 	{
+		// Læser alle linjer i filen
 		lines = Files.readAllLines(path);
 		return lines;
 	}   
--- FocusLock/src/FocusLock.java
+++ FocusLock/src/FocusLock.java
@@ -12,9 +12,12 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.binding.StringBinding;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -26,6 +29,11 @@ import javafx.util.Duration;
 
 public class FocusLock extends Application
 {
+	// variabel der holder styr på hvilket program der skal lukkes
+        String selectedProgram = "";
+        //  Liste som der skal indeholde alle lines i filen
+        ObservableList<String> lines;
+	
 	@Override
 	public void start(Stage primaryStage)
 	{
@@ -33,99 +41,91 @@ public class FocusLock extends Application
 		System.out.println(System.getProperty("user.home") + "\\Desktop\\hej.txt");
 		// Får data fra en fil ved hjælp af FileIO class
 		FileIO io = new FileIO(System.getProperty("user.home") + "/Desktop/hej.txt");
-		ObservableList<String> lines = null;
-
+        
+        
 		// Sikre at der ikke sker en fejl når vi læser data fra filen
-		try
-		{
+		try {
 			// Omdanner den liste FileIO class laver til en observable array
 			lines = FXCollections.observableArrayList(io.GenerateLineList());
-		}
-		catch (IOException ex)
-		{
+		} catch (IOException ex) {
 			Logger.getLogger(FocusLock.class.getName()).log(Level.SEVERE, null, ex);
 		}
-
+        
 		// Generer et GridPane som vi kan putte controls på
 		final GridPane root = new GridPane();
-		final Button button = new Button("CHROME KILLER");
+		// Laver en knap, med teksten "SELECT PROGRAM"
+		final Button button = new Button("SELECT PROGRAM");
 		// Laver et ListView som den inderholder alle de linjer fra Filen
-		final ListView lv = new ListView(lines);
+		final ListView lv = new ListView();
+		
+		// Tilføjer alle elementer til ListView
+		lines.forEach((line) -> {
+			lv.getItems().add(line.split(";")[1]);
+		});
 		button.setId("btn-default-lg");
-
-		// Farverne på knappen hvordan den skal ændre sig i "Timeline"
+        
+		// Farverne på knappen, hvordan den skal ændre sig i "Timeline"
 		final Color startColor = Color.web("#F6F6F6");
 		final Color endColor = Color.web("#FC6B0A");
-
+	
 		// Får farve property og gæmmer det i en color variabel
 		final ObjectProperty<Color> color = new SimpleObjectProperty<>(startColor);
-
-
+        
+        
 		final StringBinding cssColorSpec = Bindings.createStringBinding(() -> String.format("-fx-body-color: rgba(%d, %d, %d);", 
 			(int) (256*color.get().getRed()),
 			(int) (256*color.get().getGreen()),
 			(int) (256*color.get().getBlue())), color);
-
+		
 		button.styleProperty().bind(cssColorSpec);
-
+        
 		// Anim der aktiveres ved hover over knappen
 		final Timeline timeline = new Timeline(
 			new KeyFrame(Duration.ZERO, new KeyValue(color, startColor)),
 			new KeyFrame(Duration.seconds(.2), new KeyValue(color, endColor))
 		);
-
+        
 		// Anim der aktivers når der ikke længere hoves over knappen
 		final Timeline timeline2 = new Timeline(
 			new KeyFrame(Duration.ZERO, new KeyValue(color, endColor)),
 			new KeyFrame(Duration.seconds(.2), new KeyValue(color, startColor))
 		);
+        
+		// Checker når der bliver ændret i selection i ListView
+		lv.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+			@Override
+			public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+				// Debugging
+				System.out.println(lv.getItems().indexOf(newValue));
+                
+				// Sætter selectedProgram til den liste i filen
+				selectedProgram = lines.get(lv.getItems().indexOf(newValue));
+				// Ændre teksten på knappen til at være hvilket program der skal lukkes
+				button.setText(newValue.toString().toUpperCase() + " KILLER");
 
-		// Lamba function der checker om der bliver trykket på knappen
-		button.setOnAction((ActionEvent event) ->
-		{
-			// OsCheck er fra filen OsCheck.java
-			// Checker hvilken OS brugeren sidder på
-			OSCheck.OSType ostype = OSCheck.getOperatingSystemType();
-			
-			// Debugging
-			System.out.println(ostype.toString());
-			
-			// Switch case til forskellige commands der skal udføres alt efter hvilket OS man sidder på
-			switch (ostype)
-			{
-				case Windows:
+				// Debugging
+				System.out.println(selectedProgram);
+			}
+		});
+        
+		// Eventhandler der checker om der bliver trykket på knappen
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				// OsCheck er fra filen OsCheck.java
+				// Checker hvilken OS brugeren sidder på
+				OSCheck.OSType ostype = OSCheck.getOperatingSystemType();
+
+				// Debugging
+				System.out.println(ostype.toString());
+
+				// Switch case til forskellige commands der skal udføres alt efter hvilket OS man sidder på
+				switch (ostype) {
+					case Windows:
 					try
 					{
-						String[] cmd = {"taskkill","/F","/IM", "chrome.exe"};
-						// Kører den command der er givet ovenover, De første 3 argumenter er påkrævet!
-						Runtime.getRuntime().exec(cmd);
-					}
-					catch (IOException ex)
-					{
-						Logger.getLogger(FocusLock.class.getName()).log(Level.SEVERE, null, ex);
-					}
-					break;
-					
-				case MacOS:
-					try
-					{
-						String[] cmd = {"killall", "Google Chrome"};
-						// Kører den command der er givet ovenover, Det første argument er påkrævet!
-						Runtime.getRuntime().exec(cmd);
-					}
-					catch (IOException ex)
-					{
-						Logger.getLogger(FocusLock.class.getName()).log(Level.SEVERE, null, ex);
-					}
-					break;
-					
-				case Linux:
-					try
-					{
-						// IKKE TESTET ENDNU
-						button.setId("btn-default-lg-linux"); //DEBUG
-						// Skulle gerne virke, men ikke testet
-						String[] cmd = {"killall", "Google Chrome"};
+						// DEN FINDER WINDOWS KORREKT TESTET
+						String[] cmd = {"taskkill","/F","/IM", selectedProgram.split(";")[0]};
 						// Kører den command der er givet ovenover
 						Runtime.getRuntime().exec(cmd);
 					}
@@ -134,32 +134,65 @@ public class FocusLock extends Application
 						Logger.getLogger(FocusLock.class.getName()).log(Level.SEVERE, null, ex);
 					}
 					break;
-					
-				case Other:
+					case MacOS:
+					try
+					{
+						// DEN FINDER MAC KORREKT TESTET
+						String[] cmd = {"killall", selectedProgram.split(";")[1]};
+						// Kører den command der er givet ovenover
+						Runtime.getRuntime().exec(cmd);
+					}
+					catch (IOException ex)
+					{
+						Logger.getLogger(FocusLock.class.getName()).log(Level.SEVERE, null, ex);
+					}
+					break;
+					case Linux:
+					try
+					{
+						// IKKE TESTET ENDNU
+						button.setId("btn-default-lg-linux");
+						// Skulle gerne virke, men ikke testet
+						String[] cmd = {"killall", selectedProgram.split(";")[1]};
+						// Kører den command der er givet ovenover
+						Runtime.getRuntime().exec(cmd);
+					}
+					catch (IOException ex)
+					{
+						Logger.getLogger(FocusLock.class.getName()).log(Level.SEVERE, null, ex);
+					}
+					break;
+					case Other:
 					// TODO: Send en besked til vores server omkring styresystem og fejl
 					break;
+				}
+
 			}
 		});
-
+        
 		// Start hover over knappen
-		button.setOnMouseEntered((MouseEvent event) ->
-		{
-			timeline2.stop();
-			timeline.play();
+		button.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				timeline2.stop();
+				timeline.play();
+			}
 		});
-
+        
 		// Stop hover over knappen
-		button.setOnMouseExited((MouseEvent event) ->
-		{
-			timeline.stop();
-			timeline2.play();
+		button.setOnMouseExited(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				timeline.stop();
+				timeline2.play();
+			}
 		});
-
+		
 		// Tilføjer controls til pane	
 		root.add(button, 1, 0, 2, 1);
 		root.add(lv, 0, 0);
-
-		final Scene scene = new Scene(root, 420, 175);
+        
+		final Scene scene = new Scene(root, 600, 175);
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		// Loader CSS'en
